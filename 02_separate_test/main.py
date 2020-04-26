f72c1172295fa4e40f64c7fbcabbf7ed3cf35f6c
fachrulch <fachrul.fch@gmail.com>
2020-04-26 16:01:40 +0700

    Implement separate_test

--- 02_separate_test/main.py
+++ 02_separate_test/main.py
@@ -1,5 +1,10 @@
 import sys
+
 sys.path.insert(0, 'leap_year.py')
 import leap_year
 
-print("is 2020 leap year? the answer is " + str(leap_year.is_leap_year(2020)))
+
+# print("is 2020 leap year? the answer is " + str(leap_year.is_leap_year(2020)))
+years = [2017, 2018, 2019, 2020, 2021]
+for yrs in years:
+    print(f"is {yrs} leap year? that is {leap_year.is_leap_year(yrs)}")
