35288ed6ceae6660a7179b63479dc045f6086e71
fachrulch <fachrul.fch@gmail.com>
2020-05-03 22:03:20 +0700

    [selenium] parallel test completed

--- 10_selenium_parallel/tests/test_duckduckgo.py
+++ 10_selenium_parallel/tests/test_duckduckgo.py
@@ -5,11 +5,11 @@ import random
 
 class TestGoogling():
     def test_search_fachrul(self, browser:webdriver.Remote):
-        browser.get('https://google.co.id')
-        browser.find_element_by_name('q').send_keys('Fachrul Choliluddin linkedin'+ Keys.ENTER)
+        browser.get('https://duckduckgo.com/')
+        browser.find_element_by_id('search_form_input_homepage').send_keys('instagram Fachrul Choliluddin'+ Keys.ENTER)
         time.sleep(3)
         assert 'Fachrul Choliluddin' in browser.title
-        results = browser.find_elements_by_css_selector('.r a h3')
+        results = browser.find_elements_by_css_selector('h2.result__title')
         target = random.choice(results)
         print('hasil', target.text)
         target.click()
--- 10_selenium_parallel/tests/test_yahoo.py
+++ 10_selenium_parallel/tests/test_yahoo.py
@@ -7,7 +7,7 @@ import random
 class TestYahoo():
     def test_search_fachrul(self, browser: webdriver.Remote):
         browser.get('https://id.yahoo.com')
-        browser.find_element_by_id('header-search-input').send_keys('Fachrul Choliluddin' + Keys.ENTER)
+        browser.find_element_by_id('header-search-input').send_keys('github Fachrul Choliluddin' + Keys.ENTER)
         time.sleep(3)
         results = browser.find_elements_by_css_selector('h3.title')
         target = random.choice(results)
